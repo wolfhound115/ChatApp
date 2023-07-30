d91dbcd9750e0d34a4891ce97e3d7c594a098ab2
wolfhound115 <wolfhound115@gmail.com>
2023-07-30 12:49:47 -0700

    added user sign-in/sign-out buttons

--- superchat/src/App.js
+++ superchat/src/App.js
@@ -24,13 +24,41 @@ const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 function App() {
+  // if user signed in -> user is an obj
+  // if user signed out -> user is null
+  const [user] = userAuthState(auth);
+
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
 
       </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+  
+  return  (
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  )
+}
+
+function SignOut() {
+
+  return auth.currentUser && (
+
+    <button onClick={() => auth.SignOut()}>Sign Out</button>
+  )
 }
 
 export default App;
